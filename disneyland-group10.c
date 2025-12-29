12997439a0e6a15ce059257010b30c1a8e5ee0f9
Lara Khreis <lara.khreis@student.htw-berlin.de>
2025-12-29 21:54:14 +0900

    Code for the menu has been written. To be updated when sub functions are finished.

--- disneyland-group10.c
+++ disneyland-group10.c
@@ -2,7 +2,29 @@
 
 int main()
 {
+    int choice;
 
+    printf("****** Welcome to our Disneyland Reviewing System! ******\n\n");
+    printf("Please choose one option to continue by entering a number:\n\n");
 
+    char *options[] = {"1 Display Reviews", "2 Add Review", "3 Delete Review", "4 Edit Review", "5 Exit"};
+    int numberOfStrings = sizeof(options) / sizeof(options[0]);
+    
+    for (int i = 0; i < numberOfStrings; i++)
+    {
+        printf("%s", options[i]);
 
+        if (i == numberOfStrings - 1) // If last element of array...
+        {
+            printf("\n\n");          // ... add two escape sequences
+        } else
+        {
+            printf("\n");
+        }
+    }
+    
+    printf("Option: ");
+    scanf("%d", &choice);
+
+    return 0;
 }
